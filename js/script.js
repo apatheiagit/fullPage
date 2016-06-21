eb0c539578fe003203d7220385f5f4b6d2e6e5d6
apatheiagit <apatheiar@mail.ru>
2016-06-21 17:42:30 +0300

    start

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+    menu: '#menu',
+    scrollOverflow: true
+  });
+});
